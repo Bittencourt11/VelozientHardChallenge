bfab5212a11a738fe74b1fb558a7e7385d90ed75
Gabriel Bittencourt <bittencourt11us@hotmail.com>
2025-01-31 19:30:49 -0300

    Initial commit

---
+++ SmartVault.Program/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SmartVault.Program
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            //test dfsfsdfs
+
+            WriteEveryThirdFileToFile(args[0]);
+            GetAllFileSizes();
+        }
+
+        private static void GetAllFileSizes()
+        {
+            // TODO: Implement functionality
+        }
+
+        private static void WriteEveryThirdFileToFile(string accountId)
+        {
+            // TODO: Implement functionality
+        }
+    }
+}
